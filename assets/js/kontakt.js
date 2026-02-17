b87b5dab3db5dc6228b18f94ce5769ed4843737f
Julia Heigl <julia.heigl@iu-study.org>
2026-02-17 20:21:27 +0100

    Css und javasript for kontakt.html
    Css und javascript for form and success message

---
+++ assets/js/kontakt.js
@@ -0,0 +1,20 @@
+const form = document.querySelector('.kontakt-formular');      
+const nachrichtGesendet = document.querySelector('.kontakt-form-gesendet');     // Erfolgsnachricht unterhalb Formular
+
+form.addEventListener('submit', (event) => {
+    // Neuladen verhindern
+    event.preventDefault(); 
+
+    // Validierung required-Attribute und E-Mail-Adresse
+    if (form.checkValidity()) {
+        // Erfolgsnachricht anzeigen
+        nachrichtGesendet.hidden = false;
+
+        // zur Erfolgsnachricht scrollen
+        nachrichtGesendet.scrollIntoView({ behavior: 'smooth' });
+
+        // Formularfelder zurücksetzen
+        form.reset();
+
+    }
+});
