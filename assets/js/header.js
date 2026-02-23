20f0c398966171e99c6a6732f9ad01b6ae483f3b
Julia Heigl <julia.heigl@iu-study.org>
2026-02-23 20:52:28 +0100

    Add ARIA attributes for header
    ARIA attributes for main-nav in header

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,17 +1,30 @@
+// Header als web component einbinden
 class SiteHeader extends HTMLElement {
   async connectedCallback() {
     const response = await fetch('components/header.html');
     const html = await response.text();
     this.innerHTML = html;
 
+    // ARIA-Attribute setzen
     const toggle = this.querySelector('.nav-mobil');
     const nav = this.querySelector('.main-nav');
+
+    if (!toggle || !nav) return;
+
+    toggle.setAttribute('aria-expanded', 'false');
 
     toggle.addEventListener('click', () => {
       nav.classList.toggle('nav-open');
       toggle.classList.toggle('is-open');
+
+      const isOpen = toggle.classList.contains('is-open');
+      toggle.setAttribute('aria-expanded', isOpen);
     });
   }
 }
 
 customElements.define('site-header', SiteHeader);
+
+
+
+
